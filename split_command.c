d63c4c5d228398a3ae94f580a53e01d51111e3c0
Geoffrey42 <geoffreygane@gmail.com>
2017-03-27 01:55:40 +0200

    modif pour que split_command() renvoie un char * en creant make_str_from_array()

--- split_command.c
+++ split_command.c
@@ -126,11 +126,59 @@ static char *cut_word_by_delim(char **str, char **delim)
     return (dup);
 }
 
-char **split_command(char *line, char **delim)
+size_t	get_array_size(char **array)
 {
-    
+	size_t	size;
+	int	i;
+
+	size = 0;
+	i = 0;
+	while (array[i])
+		size += ft_strlen(array[i++]);
+	return (size + size - 1);
+}
+
+void	join_and_delete(char **dest, char *to_join)
+{
+	char	*del;
+
+	del = *dest;
+	*dest = ft_strjoin(*dest, to_join); 
+	ft_strdel(&del);
+}
+
+void	copy_array_in_str(char **str, char **array)
+{
+	char	*del;
+	int	i;
+
+	i = 0;
+	while (array[i])
+	{
+		join_and_delete(str, array[i]);
+		if (array[i])
+			join_and_delete(str, " ");
+		i++;
+	}
+}
+
+char	*make_str_from_array(char **array)
+{
+	char	*str;
+	size_t	size;
+
+	size = get_array_size(array);
+	if (!(str = (char *)ft_memalloc(sizeof(char) * size + 1)))
+		return (NULL);
+	copy_array_in_str(&str, array);
+	return (str);
+}
+
+char *split_command(char *line, char **delim)
+{
     int len_tab;
     char **formatted_array;
+    char *formatted_string;
     int i;
     
     len_tab = counter_argument(line, delim);
@@ -145,5 +193,6 @@ char **split_command(char *line, char **delim)
         }
     }
     formatted_array[i] = NULL;
-    return (formatted_array);
+    formatted_string = make_str_from_array(formatted_array);
+    return (formatted_string);
 }
